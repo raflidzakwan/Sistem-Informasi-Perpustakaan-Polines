b518ff3bc8ba2ae8f3c86606172fab1bf3ae700e
ahsankun <167205897+ahsankun@users.noreply.github.com>
2024-04-16 19:53:41 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<title>perpustakaan undip</title>
+<link rel="stylesheet" href="s">
+<style>
+  body {
+    background-color: #483D8B; /* Warna biru muda */
+  }
+</style>
+</head>
+<body>
+<center>
+
+<form action="login_proses.php" method="post">
+  <h2>Perpustakaan Politeknik Negeri Semarang</h2>
+  <hr>
+  <div class="imgcontainer">
+    <img src="https://web.polines.ac.id/wp-content/uploads/2022/01/Logo-Polines-96dpi-200px.png" alt="Avatar" class="avatar">
+  </div>
+
+  <div class="container">
+    <br>
+    <input type="text" placeholder="Enter Username" name="name" required><br>
+
+    <br>
+    <input type="password" placeholder="Enter Password" name="pw" required><br>
+    <br>
+    <button type="submit">Login</button><br>
+</center>
+</form>
+
+</body>
+</html>
